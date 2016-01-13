dc5d869b7e4bb63ccb9d29a21eb8e55d0ecb8c74
Andrzej Jakuszyk <ajakuszyk@gmail.com>
2016-01-13 17:37:08 +0000

    MSBuild.Microsoft.VisualStudio.Web.targets package added

--- DogTrainingWeb/Properties/AssemblyInfo.cs
+++ DogTrainingWeb/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -8,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DogTrainingWeb")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DevOpsGuys")]
 [assembly: AssemblyProduct("DogTrainingWeb")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © DevOpsGuys 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
